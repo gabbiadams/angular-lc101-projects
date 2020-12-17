d539743ac5752571030d4a339a6ecd7a7c578262
Gabbi Adams <gabbishelton@gmail.com>
2020-12-17 15:53:09 -0800

    added content

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'These Are Some Cool Photos!';
+  image1 = 'https://static.ark.org/eeuploads/adhe/Whitaker_Point.jpg';
+  image2 = 'https://cdn.thecrazytourist.com/wp-content/uploads/2019/07/ccimage-shutterstock_1098228866.jpg';
+  image3 = 'https://i.pinimg.com/originals/4d/72/96/4d72962c7edec3ef940bc5d8f59d963a.jpg';
 
   constructor() { }
 
